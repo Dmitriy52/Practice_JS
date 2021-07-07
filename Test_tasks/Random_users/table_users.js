c9ee76c619a7a6714d5b7a8f65b28df8e48ac6e9
Dmitriy52 <Diamond9496@yandex.ru>
2021-07-08 00:36:18 +0300

    created_html

--- Test_tasks/Random_users/table_users.js
+++ Test_tasks/Random_users/table_users.js
@@ -1,8 +1,9 @@
 const requestUrl = 'https://randomuser.me/api/?results=15';
 const postWrapper = document.getElementById('post__wrapper');
 let users = [];
+let trueRegistratedDate;
 
-//check git
+
 function createUsersTable(data){
     return  `
                 <div class="wrapper__post">
@@ -16,8 +17,27 @@ function createUsersTable(data){
                     </div>
                     <div>
                     <h5>Avatar:</h5>
-                    <img src="${data.picture.thumbnail}"></div>
+                    <img src="${data.picture.thumbnail}""></div>
                     </div>
+                    <div>
+                        <h5>Location:</h5>
+                         <span>${data.location.state}</span>
+                         <span>${data.location.city}</span>
+                    </div>
+                    <div>
+                        <h5>Email:</h5>
+                        <span>${data.email}</span>
+                    </div>
+                    <div>
+                        <h5>Phone:</h5>
+                        <span>${data.phone}</span>
+                    </div>
+                    <div>
+                        <h5>Registrated date:</h5>
+                        <span>${trueRegistratedDate}</span>
+                    </div>
+                    <hr />
+                </div>
             `
 }
 
@@ -26,12 +46,27 @@ function getPosts(url){
         .then(response => response.json())
         .then(json => {
             users = json.results;
-            console.log(users);
              users.forEach(user => {
-                 console.log(user);
+                 let registratedDate = user.registered.date.split("", 10);
+                 let year = [];
+                 let month = [];
+                 let day = [];
+                 for(let i = 0; i <= registratedDate.length; i++){
+                    if(i <= 3){
+                        year.push(registratedDate[i]);
+                    }
+                    if(i == 5 || i == 6 ){
+                        month.push(registratedDate[i]);
+                    }
+                    if( i == 8 || i == 9 ){
+                        day.push(registratedDate[i]);
+                     }
+                 }
+                 trueRegistratedDate = day.join("") + "." + month.join("") + "." + year.join("");
                  postWrapper.innerHTML +=createUsersTable(user);
              })
         })
 }
 
+    
 getPosts(requestUrl);
