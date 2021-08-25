b51a7a78d5135861dd40ea3497ec38c1d1180630
Dmitriy52 <Diamond9496@yandex.ru>
2021-08-26 02:26:14 +0300

    .

--- Training_tasks/Simple_tasks/Hypotenuse/hypotenuse.js
+++
@@ -1,7 +0,0 @@
-function hypotenuse(n,m){
-    n = document.getElementById('number1').value;
-    m = document.getElementById('number2').value;
-
-    let result = document.getElementById('result');
-        result.innerHTML = Math.sqrt(Math.pow(n,2) + Math.pow(m,2) );
-}
